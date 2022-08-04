bd14fd421aa2299263ebdf6126abffde258a06d8
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-08-03 22:43:54 -0400

    refactored renderLeads() to take a parameter
    Renamed as render() since it can take any array
    
    takes parameter 'leads' and using argument 'myLeads'

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") ) // c
 
 if (leadsFromLocalStorage) { // if leadsFromLocalStorage isn't 0, is truthy value
     myLeads = leadsFromLocalStorage // set myLeads equal to leadsFromLocalStorage
-    renderLeads(myLeads) // render leads into listItems
+    render(myLeads) // render leads into listItems
+                // myLeads -> passing in argument to function
 }
 
-function renderLeads(array) { //low degree of reusability -- how to make this work for different parameters
+function render(leads) { // low degree of reusability -- how to make this work for different parameters
     let listItems = ""
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${array[i]}'>
-                    ${array[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -30,14 +31,12 @@ deleteBtn.addEventListener('dblclick', () => { // or can be written as ("click",
     localStorage.clear();
     myLeads = [];
     //ulEl.innerHTML = "" this is one way to do it, or
-    renderLeads() // this will render out the empty list
+    render(myLeads) // this will render out the empty list
 });
-
-
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+    render(myLeads)
 })
