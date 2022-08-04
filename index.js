74295fb0f9d2490549cdac12bd649dc31d926954
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2022-08-03 22:07:17 -0400

    moved renderLeads() function up

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ if (leadsFromLocalStorage) {
     renderLeads()
 }
 
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
 deleteBtn.addEventListener('dblclick', () => { // or can be written as ("click", function() {
     localStorage.clear();
     myLeads = [];
@@ -26,17 +40,3 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
-}
